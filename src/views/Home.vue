8384f8f11bef40960115ae25400b7b7686d7f7b5
osacrMesh <okerekeifeanyi65@gmail.com>
2023-02-12 21:06:59 +0100

    vue-text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,27 @@
   <div>
     <h1>Home</h1>
     <p>This is an Home page</p>
-    <router-link to="/about">
+    <h3>{{ title }}</h3>
+    <button @click="changeTitle()">Chekout</button>
+    <!-- <router-link to="/about">
       <button>Go to About</button>
-    </router-link>
+    </router-link> -->
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      title: "Checkout page",
+    };
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Payment Successful";
+    },
+  },
 };
 </script>
 
